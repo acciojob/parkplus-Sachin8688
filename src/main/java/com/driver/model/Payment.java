e72478d06aceabe1377ff51a65831245ed748644
Sachin Biradar <84908560+Bsachin950@users.noreply.github.com>
2024-02-04 22:59:45 +0530

    This is last edited

--- src/main/java/com/driver/model/Payment.java
+++ src/main/java/com/driver/model/Payment.java
@@ -1,39 +1,25 @@
 package com.driver.model;
 
-
 import javax.persistence.*;
 
 @Entity
 public class Payment {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
+    private Integer id;
     private boolean paymentCompleted;
-
-    @Enumerated(EnumType.STRING)
-    private PaymentMode paymentMode;
+    @Enumerated(value = EnumType.STRING)
+    private  PaymentMode paymentMode;
 
     @OneToOne
     @JoinColumn
-    Reservation reservation;
+    private Reservation reservation;
 
-    public Payment() {
-    }
-
-    public Payment(int id, boolean paymentCompleted, PaymentMode paymentMode, Reservation reservation) {
-        this.id = id;
-        this.paymentCompleted = paymentCompleted;
-        this.paymentMode = paymentMode;
-        this.reservation = reservation;
-    }
-
-    public int getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
