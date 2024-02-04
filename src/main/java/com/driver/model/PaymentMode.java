88ad41eb9e63df7c2b94909dc0ca787dfa20eff2
Sachin Biradar <84908560+Bsachin950@users.noreply.github.com>
2024-02-04 23:15:38 +0530

    New Adding files

--- src/main/java/com/driver/model/PaymentMode.java
+++ src/main/java/com/driver/model/PaymentMode.java
@@ -1,49 +1,6 @@
 package com.driver.model;
 
-import javax.persistence.*;
+public enum PaymentMode {
 
-@Entity
-public class Payment {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    private boolean paymentCompleted;
-    @Enumerated(value = EnumType.STRING)
-    private  PaymentMode paymentMode;
-
-    @OneToOne
-    @JoinColumn
-    private Reservation reservation;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public boolean isPaymentCompleted() {
-        return paymentCompleted;
-    }
-
-    public void setPaymentCompleted(boolean paymentCompleted) {
-        this.paymentCompleted = paymentCompleted;
-    }
-
-    public PaymentMode getPaymentMode() {
-        return paymentMode;
-    }
-
-    public void setPaymentMode(PaymentMode paymentMode) {
-        this.paymentMode = paymentMode;
-    }
-
-    public Reservation getReservation() {
-        return reservation;
-    }
-
-    public void setReservation(Reservation reservation) {
-        this.reservation = reservation;
-    }
+    CASH, CARD, UPI
 }
